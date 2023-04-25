9883fdf9719bcfc1b72f449d8f689af4b8b13ed7
mponpol <mponpol@gmail.com>
2023-04-25 11:59:02 +0200

    Add layouts (Auth/Admin) , pages and routing skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+//Layout
+import LayoutAdmin from './layouts/LayoutAdmin';
+import LayoutAuth from './layouts/LayoutAuth';
+// Pages auth
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+// Pages admin
+import Home from './pages/admin/Home';
+import Error404 from './pages/Error404';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/auth" element={<LayoutAuth />}>
+          <Route index element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="/" element={<LayoutAdmin />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="*" element={<Error404 />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/layouts/LayoutAuth.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+function LayoutAuth() {
+  return (
+    <div>
+      LayoutAuth <Outlet />
+    </div>
+  );
+}
+
+export default LayoutAuth;
