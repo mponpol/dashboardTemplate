544da63da402e7dd86a8ae42b4e490eddf7d76c3
mponpol <mponpol@gmail.com>
2023-04-26 12:00:04 +0200

    Complete Sidebar menus

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,50 @@
+import { Link } from 'react-router-dom';
 import './Sidebar.css';
+import {
+  RiBarChart2Line,
+  RiEarthLine,
+  RiMessage3Line,
+  RiCalendarTodoLine,
+  RiLogoutCircleRLine,
+} from 'react-icons/ri';
 
 function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-top">
+        <h1>
+          Admin<span>.</span>
+        </h1>
+        <ul>
+          <li>
+            <Link to="/" className="sidebar-link">
+              <RiBarChart2Line className="sidebar-icon" /> Analytics
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="sidebar-link">
+              <RiEarthLine className="sidebar-icon" /> Social Media
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="sidebar-link">
+              <RiMessage3Line className="sidebar-icon" /> Messages
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="sidebar-link">
+              <RiCalendarTodoLine className="sidebar-icon" /> Schedule
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <nav className="sidebar-bottom">
+        <Link to="/" className="sidebar-link">
+          <RiLogoutCircleRLine className="sidebar-icon" /> Log out
+        </Link>
+      </nav>
+    </div>
+  );
 }
 
 export default Sidebar;
