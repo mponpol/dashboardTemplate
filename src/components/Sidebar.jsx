aff3d784f8c9582871772002821c6ff4bf934f6d
Mario Ponce <mponpol@gmail.com>
2023-04-26 07:49:20 +0200

    Create dashboard skeleton (sidebar, header, main)

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+import './Sidebar.css';
+
+function Sidebar() {
+  return <div className="sidebar">Sidebar</div>;
+}
+
+export default Sidebar;
--- src/layouts/LayoutAdmin.jsx
+++ src/layouts/LayoutAdmin.jsx
@@ -1,9 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
+import './LayoutAdmin.css';
 
 function LayoutAdmin() {
   return (
-    <div>
-      LayoutAdmin <Outlet />
+    <div className="layout-admin">
+      <Sidebar />
+      <div className="admin-main">
+        <Header />
+        <div className="admin-outlet">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
